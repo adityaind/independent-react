b9c5938157be07357a264a8802cd7cdff1f29754
Aditya_Pathak <buddingwebdeveloperind@gmail.com>
2019-09-13 16:16:54 +0530

    managing state in class based component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
+  import Person from './Person/Person';
+  import './App.css';
 
-import './App.css';
+  class App extends Component {
+    state= {
+      persons:[
+      {name : 'adi', age:26},
+      {name : 'pat', age:27}
+      ]
+    }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi, I am a react app</h1>
-      </div>
-    );
+    switchNameHandler = () => {
+      this.setState({
+      persons:[
+      {name : 'adity', age:29},
+      {name : 'pat', age:30},
+     
+      ]
+    })
+    }
+    render() {
+      return (
+        <div className="App">
+        <h1>Hi, I am a react app</h1>
+        <p>This is working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age = {this.state.persons[1].age}/>
+    
+        </div>
+        );
+    }
   }
-}
 
-export default App;
+  export default App;
