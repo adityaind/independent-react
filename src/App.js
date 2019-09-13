9b3b4e331c5999fd5d09cdd84cf4b63f852e2af2
Aditya_Pathak <buddingwebdeveloperind@gmail.com>
2019-09-13 16:34:32 +0530

    using state in functional component by useState hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-  import React, { Component } from 'react';
+  import React, { useState } from 'react';
   import Person from './Person/Person';
   import './App.css';
 
-  class App extends Component {
-    state= {
+  const App =props =>{
+    const [personState, setPersonState] = useState({
       persons:[
       {name : 'adi', age:26},
       {name : 'pat', age:27}
       ]
-    }
+    })
 
-    switchNameHandler = () => {
-      this.setState({
+    //const[otherState, setOtherState] = useState('some other value');
+    
+    console.log(personState);
+
+    const switchNameHandler = () => {
+      setPersonState({
       persons:[
       {name : 'adity', age:29},
       {name : 'pat', age:30},
-     
+
       ]
     })
     }
-    render() {
+    
       return (
         <div className="App">
         <h1>Hi, I am a react app</h1>
         <p>This is working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age = {this.state.persons[1].age}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person
+         name = {personState.persons[0].name}
+        age = {personState.persons[0].age}/>
+        <Person
+         name={personState.persons[1].name}
+         age = {personState.persons[1].age}/>
     
         </div>
         );
     }
-  }
+  
 
   export default App;
